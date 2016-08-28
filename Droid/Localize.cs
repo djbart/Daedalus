1690ebdcb614cbb8f79829aa30a71c2e94f6c539
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2016-08-28 12:07:51 +0200

    Added android translation extension

---
+++ Droid/Localize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(TheDaedalusSentenceCompanion.Droid.Localize))]
+
+namespace TheDaedalusSentenceCompanion.Droid
+{
+	public class Localize : TheDaedalusSentenceCompanion.ILocalize
+	{
+		public System.Globalization.CultureInfo GetCurrentCultureInfo()
+		{
+			var androidLocale = Java.Util.Locale.Default;
+			var netLanguage = androidLocale.ToString().Replace("_", "-");
+			return new System.Globalization.CultureInfo(netLanguage);
+		}
+	}
+}
